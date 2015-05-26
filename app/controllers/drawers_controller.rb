c03614feaa04e17d1ad02a1fb02508162216c15c
memoxmrdl <jmemox@gmail.com>
2015-05-25 19:15:16 -0500

    Cargar index, new y create de drawers

--- app/controllers/drawers_controller.rb
+++ app/controllers/drawers_controller.rb
@@ -1,4 +1,25 @@
 class DrawersController < ApplicationController
   def index
+    @drawers = Drawer.all
+  end
+
+  def new
+    @drawer = Drawer.new
+  end
+
+  def create
+    @drawer = Drawer.new(drawer_params)
+
+    if @drawer.save
+      redirect_to drawers_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def drawer_params
+    params.require(:drawer).permit(:name)
   end
 end
