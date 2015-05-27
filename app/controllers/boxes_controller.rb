cf5df1336daffd18104426ed3475fe1bc672e712
memoxmrdl <jmemox@gmail.com>
2015-05-26 20:15:15 -0500

    WIP

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -10,13 +10,36 @@ class BoxesController < ApplicationController
   def create
     @box = Box.new(box_params)
 
-    @box.valid?
-    puts ">>>> #{@box.errors.messages}"
     if @box.save
       redirect_to drawers_path, notice: 'Nicho creado correctamente'
     else
-      flash.now[:alert] = 'Nicho pudo ser creado correctamente'
+      flash.now[:alert] = 'Nicho pudo ser creado'
       render :new
+    end
+  end
+
+  def edit
+    @box = Box.find_by_slug(params[:id])
+  end
+
+  def update
+    @box = Box.find_by_slug(params[:id])
+
+    if @box.update_attributes(box_params)
+      redirect_to drawer_box_path(@box.drawer, @box), notice: 'Nicho actualizado correctamente'
+    else
+      flash.now[:alert] = 'Nicho pudo ser actualizado'
+      render :edit
+    end
+  end
+
+  def destroy
+    @box = Box.find_by_slug(params[:id])
+
+    if @box.destroy
+      redirect_to drawers_path, notice: 'Nicho eliminado correctamente'
+    else
+      redirect_to drawers_path, alert: 'No se encontro ningun nicho'
     end
   end
 
